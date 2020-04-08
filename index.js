9183baf5d02aa68e9db2668314ad3d8c79eee3ee
Olia1011 <olgamelnyk10111989@gmail.com>
2020-04-08 12:34:06 +0300

    last commit2

--- index.js
+++ index.js
@@ -19,13 +19,8 @@ class CountdownTimer {
     this.timerStart = setInterval(() => {
       const currentDate = new Date();
       const time = this.targetDate - currentDate;
-      const days = String(Math.floor(time / (1000 * 60 * 60 * 24))).padStart(
-        2,
-        '0',
-      );
-      const hours = pad(
-        Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-      );
+      const days = String(Math.floor(time / (1000 * 60 * 60 * 24)));
+      const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)), );
       const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
       const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
       updateTimerFace(days, hours, mins, secs);
